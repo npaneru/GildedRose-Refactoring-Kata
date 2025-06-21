89a65b8abbb6e418bf295b48263337648fc6d37d
Naresh Chander <paneru.n@gmail.com>
2025-06-21 15:23:47 +0530

    Added logic

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -17,15 +17,15 @@ class AgedBrieUpdater(ItemUpdater):
 
 class BackstagePassUpdater(ItemUpdater):
     def update(self, item):
+        
+        item.quality = min(50, item.quality + 1)
+        if item.sell_in < 11:
+            item.quality = item.quality + 1
+        if item.sell_in < 6:
+            item.quality = item.quality + 1
+        item.sell_in = item.sell_in -1
         if item.sell_in < 0:
             item.quality = 0
-        elif item.sell_in < 5:
-            item.quality = item.quality + 3
-        elif item.sell_in < 10:
-            item.quality = item.quality + 2
-        else:
-            item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
 
 class SulfurasUpdater(ItemUpdater):
     def update(self, item):
