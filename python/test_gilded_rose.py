d784bcc46f7dce1c8f3619c4929e661b8063d62f
Naresh Chander <paneru.n@gmail.com>
2025-06-21 14:20:56 +0530

    Added few test cases

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -11,6 +11,73 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
 
+    def test_AgedBrie(self):
+        items = [Item("Aged Brie", 10, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(11, items[0].quality)
+
+    def test_aged_quality_lt_50(self):
+        items = [Item("Aged Brie", 10, 45)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(46, items[0].quality)
+
+    def test_aged_sell_in_lt_0_qual_lt_50(self):
+        items = [Item("Aged Brie", -1, 45)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-2, items[0].sell_in)
+        self.assertEqual(47, items[0].quality) # 2 increased when sell_in < 0 & qual <50
+
+    def test_aged_sell_in_lt_0_qual_gt_50(self):
+        items = [Item("Aged Brie", -1, 55)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-2, items[0].sell_in)
+        self.assertEqual(55, items[0].quality)
+
+    def test_aged_sell_in_gt_0_qual_lt_50(self):
+        items = [Item("Aged Brie", 3, 45)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(2, items[0].sell_in)
+        self.assertEqual(46, items[0].quality) # 2 increased when sell_in < 0 & qual <50
+
+    def test_aged_sell_in_gt_0_qual_gt_50(self):
+        items = [Item("Aged Brie", 3, 55)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(2, items[0].sell_in)
+        self.assertEqual(55, items[0].quality) # No change when qual > 50
+
+    # Sulfuras no changes
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", -1, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(10, items[0].quality)
+
+    def test_backstage_pass_increase(self):
+        items = [
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)
+        ]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEqual(items[0].quality, 21)  # +1
+        self.assertEqual(items[1].quality, 22)  # +2
+        self.assertEqual(items[2].quality, 23)  # +3
+
+    def test_backstage_pass_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEqual(items[0].quality, 0)
         
 if __name__ == '__main__':
     unittest.main()
